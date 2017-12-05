a69cc84b094dc4414b3b891fd2d011afea7b4449
Lee Offir <ljotyd@mail.missouri.edu>
2017-12-04 18:13:30 -0600

    Database
    database

--- final.sql
+++ final.sql
@@ -1,32 +1,11 @@
--- MySQL dump 10.13  Distrib 5.1.51, for pc-linux-gnu (i686)
---
--- Host: 127.0.0.1    Database: world
--- ------------------------------------------------------
--- Server version       5.1.51-debug-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES latin1 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 DROP SCHEMA IF EXISTS epiz_20676241_world;
 CREATE SCHEMA epiz_20676241_world;
 USE epiz_20676241_world;
 SET AUTOCOMMIT=0;
 
---
--- Table structure for table `city`
---
 
 DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `customer` (
   `CustomerID` INT(10) NOT NULL AUTO_INCREMENT,
   `FirstName` CHAR(35) NOT NULL DEFAULT '',
@@ -35,12 +14,7 @@ CREATE TABLE `customer` (
   KEY `OrderID` (`OrderID`),
   CONSTRAINT `city_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `customer` (`Code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4080 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `city`
---
---  BY:  `ID`
 
 INSERT INTO `customer` VALUES (1,'John','Smith');
 INSERT INTO `customer` VALUES (2,'Patrick','Waganar');
@@ -49,13 +23,10 @@ INSERT INTO `customer` VALUES (4,'Debra','Ulysess');
 INSERT INTO `customer` VALUES (5,'Steve','Madden');
 INSERT INTO `customer` VALUES (6,'Allison','Joy');
 
---
--- Table structure for table `country`
---
+
 
 DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `product` (
   `ProductID` INT(10) NOT NULL AUTO_INCREMENT '',
   `GameName` CHAR(52) NOT NULL DEFAULT '',
@@ -67,13 +38,6 @@ CREATE TABLE `product` (
   KEY `OrderID` (`OrderID`),
   CONSTRAINT `city_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `customer` (`Code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4080 DEFAULT CHARSET=latin1;
-
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `country`
---
---  BY:  `Code`
 
 INSERT INTO `product` VALUES (1,'Counter Strike: Global Offenseive','Valve','Valve',2012, 14.99);
 INSERT INTO `product` VALUES (2,'Rocket League','Psyonix','Psyonix',2015, 19.99);
@@ -93,12 +57,9 @@ INSERT INTO `product` VALUES (17, 'Goat Simulator','Coffee Stain Studios','Coffe
 INSERT INTO `product` VALUES (18,'Portal 2','Valve','Valve',2011, 19.99);
 INSERT INTO `product` VALUES (19, 'Watch_Dogs','Ubisoft','Ubisoft',2014, 29.99);
 INSERT INTO `product` VALUES (20, 'Dota','Valve','Valve',2013, 0.00);
--- Table structure for table ``
---
+
 
 DROP TABLE IF EXISTS `Order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Order` (
   `OrderID` INT(10) NOT NULL AUTO_INCREMENT '',
   `CustomerID` CHAR(30) NOT NULL DEFAULT '',
@@ -108,31 +69,14 @@ CREATE TABLE `Order` (
   KEY `OrderID` (`OrderID`),
   CONSTRAINT `_ibfk_1` FOREIGN KEY (`CustomerID`, `ProductID`) REFERENCES `country` (`Code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table ``
---
---  BY:  `CountryCode`,`Language`
 
-INSERT INTO `Order` VALUES (1,1,'Smith');
-INSERT INTO `Order` VALUES (2,2,'Patrick','Waganar');
-INSERT INTO `Order` VALUES (3,3,'Samuel','Slayer');
-INSERT INTO `Order` VALUES (4,4,'Debra','Ulysess');
-INSERT INTO `Order` VALUES (5,5,'Steve','Madden');
-INSERT INTO `Order` VALUES (6,6,'Allison','Joy');
+INSERT INTO `Order` VALUES (1,1,10,59.99);
+INSERT INTO `Order` VALUES (2,2,13,19.99);
+INSERT INTO `Order` VALUES (3,3,20,0.00);
+INSERT INTO `Order` VALUES (4,4,7,9.99);
+INSERT INTO `Order` VALUES (5,5,9,9.99);
+INSERT INTO `Order` VALUES (6,6,8,29.99);
 
 
 SET AUTOCOMMIT=1;
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-09-30 11:01:37
