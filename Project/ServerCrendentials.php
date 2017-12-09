e5ed3b15331887afcc9390ce88b57ed93f8835a3
Kolbe Weathington <kswof97@yahoo.com>
2017-12-08 18:46:49 -0600

    Credentials Tweak/Remove PHP File added

--- Project/ServerCrendentials.php
+++ Project/ServerCrendentials.php
@@ -1,7 +1,9 @@
+<?php
     $servername = "sql3.freesqldatabase.com";
     $username = "sql3209420";
     $password = "DPHvgZgvvd";
     $dbname = "sql3209420";
-    
+
     Databases (so far) are:
     Customer, Product, Orders
+?>
